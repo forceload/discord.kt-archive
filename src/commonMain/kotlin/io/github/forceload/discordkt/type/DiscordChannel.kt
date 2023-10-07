7ad1924fa6070f70e7b6f8704b96c776efa0b3f8
컨트롤D <controld3785@gmail.com>
2023-10-07 13:38:35 +0900

    Finish DiscordChannel deserialize method

--- src/commonMain/kotlin/io/github/forceload/discordkt/type/DiscordChannel.kt
+++ src/commonMain/kotlin/io/github/forceload/discordkt/type/DiscordChannel.kt
@@ -1,6 +1,8 @@
 package io.github.forceload.discordkt.type
 
 import io.github.forceload.discordkt.channel.DiscordChannelType
+import io.github.forceload.discordkt.util.SerializerExtension.arraySerializer
+import io.github.forceload.discordkt.util.SerializerExtension.decodeNullableString
 import io.github.forceload.discordkt.util.SerializerUtil.makeStructure
 import kotlinx.datetime.Instant
 import kotlinx.datetime.serializers.InstantIso8601Serializer
@@ -8,30 +10,31 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.*
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
-@Serializable(with = ChannelOverwriteType.Serializer::class)
-enum class ChannelOverwriteType(val id: Int) {
+@Serializable(with = PermissionOverwriteType.Serializer::class)
+enum class PermissionOverwriteType(val id: Int) {
     ROLE(0), MEMBER(1);
 
     companion object {
         fun fromID(id: Int) = entries.first { it.id == id }
     }
 
-    object Serializer: KSerializer<ChannelOverwriteType> {
+    object Serializer: KSerializer<PermissionOverwriteType> {
         override val descriptor: SerialDescriptor =
             PrimitiveSerialDescriptor("ChannelOverwriteType", PrimitiveKind.INT)
 
-        override fun deserialize(decoder: Decoder) = ChannelOverwriteType.fromID(decoder.decodeInt())
-        override fun serialize(encoder: Encoder, value: ChannelOverwriteType) = encoder.encodeInt(value.id)
+        override fun deserialize(decoder: Decoder) = PermissionOverwriteType.fromID(decoder.decodeInt())
+        override fun serialize(encoder: Encoder, value: PermissionOverwriteType) = encoder.encodeInt(value.id)
     }
 }
 
 @Serializable(with = PermissionOverwrite.Serializer::class)
 class PermissionOverwrite(
-    val id: String, val type: ChannelOverwriteType,
+    val id: String, val type: PermissionOverwriteType,
     val allow: Set<DiscordPermission>, val deny: Set<DiscordPermission>
 ) {
     object Serializer: KSerializer<PermissionOverwrite> {
@@ -45,14 +48,14 @@ class PermissionOverwrite(
 
         override fun deserialize(decoder: Decoder): PermissionOverwrite {
             var id: String? = null
-            var type: ChannelOverwriteType? = null
+            var type: PermissionOverwriteType? = null
             var allow: Set<DiscordPermission>? = null
             var deny: Set<DiscordPermission>? = null
 
             decoder.makeStructure(descriptor) { index ->
                 when (index) {
                     0 -> id = decodeStringElement(descriptor, index)
-                    1 -> type = decodeSerializableElement(descriptor, index, ChannelOverwriteType.Serializer)
+                    1 -> type = decodeSerializableElement(descriptor, index, PermissionOverwriteType.Serializer)
                     2 -> allow = decodeSerializableElement(descriptor, index, DiscordPermission.SetSerializer)
                     3 -> deny = decodeSerializableElement(descriptor, index, DiscordPermission.SetSerializer)
                 }
@@ -64,7 +67,7 @@ class PermissionOverwrite(
         override fun serialize(encoder: Encoder, value: PermissionOverwrite) {
             encoder.beginStructure(descriptor).run {
                 encodeStringElement(descriptor, 0, value.id)
-                encodeSerializableElement(descriptor, 1, ChannelOverwriteType.Serializer, value.type)
+                encodeSerializableElement(descriptor, 1, PermissionOverwriteType.Serializer, value.type)
                 encodeSerializableElement(descriptor, 2, DiscordPermission.SetSerializer, value.allow)
                 encodeSerializableElement(descriptor, 3, DiscordPermission.SetSerializer, value.deny)
                 endStructure(descriptor)
@@ -101,10 +104,10 @@ class ThreadMetadata(
             buildClassSerialDescriptor("ThreadMetadata") {
                 element<Boolean>("archived")
                 element<Int>("auto_archive_duration")
-                element<Instant>("archive_timestamp")
+                element<String>("archive_timestamp")
                 element<Boolean>("locked")
                 element<Boolean>("invitable", isOptional = true)
-                element<Instant?>("create_timestamp", isOptional = true)
+                element<String?>("create_timestamp", isOptional = true)
             }
 
         @OptIn(ExperimentalSerializationApi::class)
@@ -159,8 +162,8 @@ class ThreadMember(val id: String? = null, val userID: String? = null, val joinT
             buildClassSerialDescriptor("ThreadMember") {
                 element<String>("id", isOptional = true)
                 element<String>("user_id", isOptional = true)
-                element<Instant>("join_timestamp")
-                element<Int>("flags") // TODO("Find Usage: Unknown")
+                element<String>("join_timestamp")
+                element<Int>("flags") // TODO: Find Usage
                 element<GuildMember>("member", isOptional = true)
             }
 
@@ -247,6 +250,33 @@ enum class ForumLayoutType(val id: Int) {
     }
 }
 
+enum class ChannelFlags(val id: Int) {
+    PINNED(1 shl 1), REQUIRE_TAG(1 shl 4), HIDE_MEDIA_DOWNLOAD_OPTIONS(1 shl 15);
+
+    object SetSerializer: KSerializer<Set<ChannelFlags>> {
+        override val descriptor: SerialDescriptor =
+            PrimitiveSerialDescriptor("ChannelFlags", PrimitiveKind.INT)
+
+        override fun deserialize(decoder: Decoder): Set<ChannelFlags> {
+            val intentFlag = decoder.decodeInt()
+
+            val flagSet = mutableSetOf<ChannelFlags>()
+            entries.forEach {
+                if (intentFlag and it.id == it.id) flagSet.add(it)
+            }
+
+            return flagSet
+        }
+
+        override fun serialize(encoder: Encoder, value: Set<ChannelFlags>) {
+            var result = 0
+            value.forEach { result = result or it.id }
+
+            encoder.encodeInt(result)
+        }
+    }
+}
+
 /**
  * https://discord.com/developers/docs/resources/channel#channel-object
  */
@@ -260,7 +290,7 @@ class DiscordChannel(
     val managed: Boolean? = null, val parentID: String? = null, val lastPinTimestamp: Instant? = null,
     val rtcRegion: String? = null, val videoQualityMode: VideoQualityMode? = VideoQualityMode.AUTO, val messageCount: Int? = null,
     val memberCount: Int? = null, val threadMetadata: ThreadMetadata? = null, val member: ThreadMember? = null,
-    val defaultAutoArchiveDuration: Int? = null, val permissions: Set<DiscordPermission>, val flags: Int? = null,
+    val defaultAutoArchiveDuration: Int? = null, val permissions: Set<DiscordPermission>, val flags: Set<ChannelFlags>? = null,
     val totalMessageSent: Int? = null, val availableTags: Array<ForumTag>? = null, val appliedTags: Array<String>? = null,
     val defaultReactionEmoji: DefaultReaction? = null, val defaultThreadRateLimitPerUser: Int? = null,
     val defaultSortOrder: SortOrderType? = null, val defaultForumLayout: ForumLayoutType? = ForumLayoutType.NOT_SET
@@ -268,15 +298,144 @@ class DiscordChannel(
     object Serializer: KSerializer<DiscordChannel> {
         override val descriptor: SerialDescriptor =
             buildClassSerialDescriptor("DiscordChannel") {
-
+                element<String>("id")
+                element<DiscordChannelType>("type") // Experiment
+                element<String>("guild_id", isOptional = true)
+                element<Int>("position")
+                element<Array<PermissionOverwrite>>("permission_overwrites", isOptional = true)
+                element<String?>("name", isOptional = true)
+                element<String?>("topic", isOptional = true)
+                element<Boolean>("nsfw", isOptional = true)
+                element<String?>("last_message_id", isOptional = true)
+                element<Int>("bitrate", isOptional = true)
+                element<Int>("user_limit", isOptional = true)
+                element<Int>("rate_limit_per_user", isOptional = true)
+                element<Array<DiscordUser>>("recipients", isOptional = true)
+                element<String?>("icon", isOptional = true)
+                element<String>("owner_id", isOptional = true)
+                element<String>("application_id", isOptional = true)
+                element<Boolean>("managed", isOptional = true)
+                element<String?>("parent_id", isOptional = true)
+                element<String?>("last_pin_timestamp", isOptional = true)
+                element<String?>("rtc_region", isOptional = true)
+                element<Int>("video_quality_mode", isOptional = true)
+                element<Int>("message_count", isOptional = true)
+                element<Int>("member_count", isOptional = true)
+                element<ThreadMetadata>("thread_metadata", isOptional = true)
+                element<ThreadMember>("member", isOptional = true)
+                element<Int>("default_auto_archive_duration", isOptional = true)
+                element<String>("permissions", isOptional = true)
+                element<Int>("flags", isOptional = true)
+                element<Int>("total_message_sent", isOptional = true)
+                element<Array<ForumTag>>("available_tags", isOptional = true)
+                element<Array<String>>("applied_tags", isOptional = true)
+                element<DefaultReaction?>("default_reaction_emoji", isOptional = true)
+                element<Int>("default_thread_rate_limit_per_user", isOptional = true)
+                element<SortOrderType?>("default_sort_order", isOptional = true) // Experiment
+                element<ForumLayoutType>("default_forum_layout", isOptional = true)
             }
 
+        @OptIn(ExperimentalSerializationApi::class)
         override fun deserialize(decoder: Decoder): DiscordChannel {
-            decoder.makeStructure(descriptor) {
+            var id: String? = null
+            var type: DiscordChannelType? = null
+            var guildID: String? = null
+            var position: Int? = null
+            var permissionOverwrites = arrayOf<PermissionOverwrite>()
+            var name: String? = null
+            var topic: String? = null
+            var nsfw: Boolean? = null
+            var lastMessageID: String? = null
+            var bitrate: Int? = null
+            var userLimit: Int? = null
+            var rateLimitPerUser: Int? = null
+            var recipients: Array<DiscordUser>? = null
+            var icon: String? = null
+            var ownerID: String? = null
+            var appID: String? = null
+            var managed: Boolean? = null
+            var parentID: String? = null
+            var lastPinTimestamp: Instant? = null
+            var rtcRegion: String? = null
+            var videoQualityMode: VideoQualityMode? = null
+            var messageCount: Int? = null
+            var memberCount: Int? = null
+            var threadMetadata: ThreadMetadata? = null
+            var member: ThreadMember? = null
+            var defaultAutoArchiveDuration: Int? = null
+            var permissions: Set<DiscordPermission> = setOf()
+            var flags: Set<ChannelFlags>? = null
+            var totalMessageSent: Int? = null
+            var availableTags: Array<ForumTag>? = null
+            var appliedTags: Array<String>? = null
+            var defaultReactionEmoji: DefaultReaction? = null
+            var defaultThreadRateLimitPerUser: Int? = null
+            var defaultSortOrder: SortOrderType? = null
+            var defaultForumLayout: ForumLayoutType? = null
+            decoder.makeStructure(descriptor) { index ->
+                when (index) {
+                    0 -> id = decodeStringElement(descriptor, index)
+                    1 -> type = decodeSerializableElement(descriptor, index, DiscordChannelType.Serializer)
+                    2 -> guildID = decodeStringElement(descriptor, index)
+                    3 -> position = decodeIntElement(descriptor, index)
+                    4 -> permissionOverwrites =
+                        decodeSerializableElement(descriptor, index, PermissionOverwrite.Serializer.arraySerializer())
 
+                    5 -> name = decodeNullableString(descriptor, index)
+                    6 -> topic = decodeNullableString(descriptor, index)
+                    7 -> nsfw = decodeBooleanElement(descriptor, index)
+                    8 -> lastMessageID = decodeNullableString(descriptor, index)
+                    9 -> bitrate = decodeIntElement(descriptor, index)
+                    10 -> userLimit = decodeIntElement(descriptor, index)
+                    11 -> rateLimitPerUser = decodeIntElement(descriptor, index)
+                    12 -> recipients =
+                        decodeSerializableElement(descriptor, index, DiscordUser.Serializer.arraySerializer())
+
+                    13 -> icon = decodeNullableString(descriptor, index)
+                    14 -> ownerID = decodeStringElement(descriptor, index)
+                    15 -> appID = decodeStringElement(descriptor, index)
+                    16 -> managed = decodeBooleanElement(descriptor, index)
+                    17 -> parentID = decodeNullableString(descriptor, index)
+                    18 -> lastPinTimestamp =
+                        decodeNullableSerializableElement(descriptor, index, InstantIso8601Serializer)
+
+                    19 -> rtcRegion = decodeNullableString(descriptor, index)
+                    20 -> videoQualityMode = decodeSerializableElement(descriptor, index, VideoQualityMode.Serializer)
+                    21 -> messageCount = decodeIntElement(descriptor, index)
+                    22 -> memberCount = decodeIntElement(descriptor, index)
+                    23 -> threadMetadata = decodeSerializableElement(descriptor, index, ThreadMetadata.Serializer)
+                    24 -> member = decodeSerializableElement(descriptor, index, ThreadMember.Serializer)
+                    25 -> defaultAutoArchiveDuration = decodeIntElement(descriptor, index)
+                    26 -> permissions = decodeSerializableElement(descriptor, index, DiscordPermission.SetSerializer)
+                    27 -> flags = decodeSerializableElement(descriptor, index, ChannelFlags.SetSerializer)
+                    28 -> totalMessageSent = decodeIntElement(descriptor, index)
+                    29 -> availableTags =
+                        decodeSerializableElement(descriptor, index, ForumTag.serializer().arraySerializer())
+
+                    30 -> appliedTags =
+                        decodeSerializableElement(descriptor, index, String.serializer().arraySerializer())
+
+                    31 -> defaultReactionEmoji =
+                        decodeNullableSerializableElement(descriptor, index, DefaultReaction.serializer())
+
+                    32 -> defaultThreadRateLimitPerUser = decodeIntElement(descriptor, index)
+                    33 -> defaultSortOrder =
+                        decodeNullableSerializableElement(descriptor, index, SortOrderType.Serializer)
+
+                    34 -> defaultForumLayout = decodeSerializableElement(descriptor, index, ForumLayoutType.Serializer)
+                }
             }
 
-            TODO("야발 이걸 어케해")
+            return DiscordChannel(
+                id!!, type!!, guildID, position,
+                permissionOverwrites, name, topic, nsfw,
+                lastMessageID, bitrate, userLimit, rateLimitPerUser, recipients,
+                icon, ownerID, appID, managed, parentID, lastPinTimestamp, rtcRegion,
+                videoQualityMode, messageCount, memberCount,
+                threadMetadata, member, defaultAutoArchiveDuration, permissions, flags,
+                totalMessageSent, availableTags, appliedTags, defaultReactionEmoji,
+                defaultThreadRateLimitPerUser, defaultSortOrder, defaultForumLayout
+            )
         }
 
         override fun serialize(encoder: Encoder, value: DiscordChannel) {
